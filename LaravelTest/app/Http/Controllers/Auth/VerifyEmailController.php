a4401b58e0935f65067574c25085611de2e110ef
Santiago Lorenzano <enderwigim@gmail.com>
2025-09-30 17:38:57 +0200

    Now pedidos and clientes are different routes

--- LaravelTest/app/Http/Controllers/Auth/VerifyEmailController.php
+++ LaravelTest/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -11,14 +11,14 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
-    {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
-        }
+    // public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    // {
+    //     if ($request->user()->hasVerifiedEmail()) {
+    //         //return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+    //     }
 
-        $request->fulfill();
+    //     $request->fulfill();
 
-        return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
-    }
+    //     //return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+    // }
 }
